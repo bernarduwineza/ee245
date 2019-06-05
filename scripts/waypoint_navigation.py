44ca4eefc4968cd94239125c53b5c889732c6989
bernarduwineza <bernarduwineza@gmail.com>
2019-06-05 02:47:45 -0700

    circle and figure 5 maneuvers

--- scripts/waypoint_navigation.py
+++ scripts/waypoint_navigation.py
@@ -22,9 +22,74 @@ if __name__ == '__main__':
 
     # FILL IN YOUR CODE HERE
     # Please try both goTo and cmdPosition
+   
+    # 2-D line maneuver
+    tmp = np.add(initialPosition, [0.0, 0.0, 1.0])    
+    cf.goTo(goal=tmp, yaw=0, duration=5.0)  
+    time.sleep(5.0) 
 
+    tmp = np.add(initialPosition, [1.8, 0.0, 1.0])
+    cf.goTo(goal=tmp, yaw=0, duration=5.0)
+    time.sleep(5.0)
 
     
+    tmp = np.add(initialPosition, [1.8, 0.5, 1.0])
+    cf.goTo(goal=tmp, yaw=0, duration=2.0)
+    time.sleep(5.0)
+    
+   
+    tmp = np.add(initialPosition, [0.0, 0.5, 1.0])
+    cf.goTo(goal=tmp, yaw=0, duration=5.0)
+    time.sleep(5.0)
+    
+    
+    tmp = np.add(initialPosition, [0.0, 0.0, 1.0])
+    cf.goTo(goal=tmp, yaw=0, duration=2.0)
+    time.sleep(5.0)  
+
+    # 2-D Circle maneuver
+    
+    def circle_points(r, n):
+        circles  = []
+        zeros = np.zeros(n)
+        t = np.linspace(0, 2*np.pi, n)
+        x = r * np.cos(t)
+        y = r * np.sin(t)
+        circles  = np.c_[x, y, zeros]
+
+        return circles 
+    n = 20	# number of points 
+    t = 5/n	# time to the next point in a circle
+    circ = circle_points(0.45, n)
+    print(tmp)
+    for i in range(n):
+        pt = np.add(circ[i], tmp)
+        cf.cmdPosition(pos=pt, yaw=0)
+        time.sleep(0.4)
+  
+    time.sleep(5.0)
+
+    # Eight curve maneuver
+    def eight_points(r, n):
+        eight  = []
+        zeros = np.zeros(n)
+        t = np.linspace(0, 2*np.pi, n)
+        x = r * np.cos(t)
+        y = r * np.sin(t) * np.cos(t)
+        eight  = np.c_[x, y, zeros]
+        return eight 
+
+    n = 40	# number of points 
+    t = 5/n	# time to the next point in the curve
+    eight = eight_points(1.7, n)
+    print(eight)
+
+    for i in range(n):
+        pt = np.add(eight[i], tmp)
+        cf.cmdPosition(pos=pt, yaw=0)
+        time.sleep(0.2)
+    
+    time.sleep(5.0)
 
     cf.land(targetHeight = 0.0, duration = 5.0)
     time.sleep(5.0)
