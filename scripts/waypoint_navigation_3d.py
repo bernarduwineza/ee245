e2a298220e019deddb2ad18a8f77ef2756cfe833
Akila Ganlath <akila.ganlath@gmail.com>
2019-06-07 14:01:14 -0700

    updated transform

--- scripts/waypoint_navigation_3d.py
+++ scripts/waypoint_navigation_3d.py
@@ -30,15 +30,21 @@ if __name__ == '__main__':
         return eightPlanar 
 
     def transform(theta,p,q0):
-        T = np.
-
+        # transforms the vector `q0` by shifting by `p` and rotating about the `x` axis of the world frame by `theta`
+        p = np.reshape(p,(3,1))
+        R = np.array([[1 0 0],[0 np.cos(theta) -np.sin(theta)],[0 np.sin(theta) np.cos(theta)]])
+        T1 = np.hstack((R,p))
+        T2 = np.hstack((zeros(3),1))
+        T = np.vstack((T1,T2))
+        q1 = np.linalg.multiply(T,q0)
 
     # Preliminaries
-    n = 200	                 number of points 
+    n = 200	                # number of points 
     t = 5/n	                # [sec] time to the next point in the curve
     r = .75                 # [meters]
     height = 0.5            # [meters]
     gotoDuration = 3.0      # [sec] time for goto tracking 
+    theta = np.pi/8         # [rad] angle to til trajectory about the world frame x
 
     # define trajectory
     eight_3d = eight3d_points(r, n)
