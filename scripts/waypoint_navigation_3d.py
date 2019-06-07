fb4f78e5b4c7357bbc6e64f3bcf12a847a1a3511
di55olve88 <aganl001@ls>
2019-06-07 12:57:02 -0700

    new file for 3d test; optimized for runtime

---
+++ scripts/waypoint_navigation_3d.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+import math
+import numpy as np
+from crazyflieParser import CrazyflieParser
+
+if __name__ == '__main__':
+
+    index = 1   # for cf1
+    initialPosition = [0,1.5,0] # x,y,z coordinate for this crazyflie
+    cfs = CrazyflieParser(index, initialPosition)
+    cf = cfs.crazyflies[0]
+    time = cfs.timeHelper
+
+    cf.setParam("commander/enHighLevel", 1)
+    cf.setParam("stabilizer/estimator", 2) # Use EKF
+    cf.setParam("stabilizer/controller", 2) # Use mellinger controller
+    #cf.setParam("ring/effect", 7)
+
+    # 3D Figure 8 -------------------------------------------
+    
+    # Eight curve maneuver 3D
+    def eight3d_points(r, n):
+        eight_3d  = []
+        t = np.linspace(-(np.pi), np.pi, n)
+        x = 0.2*t
+        y = r * np.cos(t)
+        z = r * np.sin(t) * np.cos(t)
+        eight_3d  = np.c_[x, y, z]
+        return eight_3d 
+
+    # Preliminaries
+    n = 200	    # number of points 
+    t = 5/n	    # [sec] time to the next point in the curve
+    r = .75     # [meters]
+
+    eight_3d = eight3d_points(r, n)
+
+    # takeoff to height
+    cf.takeoff(targetHeight = 0.5, duration = 3.0)
+    time.sleep(3.0)
+
+    # navigate to tangent of curve 
+    tmp = np.add(eight_3d[0], [0,0,1])
+    cf.goTo(goal=tmp, yaw=0, duration=3.0)
+    time.sleep(3.0)
+
+    for i in range(n):
+        if i == 0: 
+            pt = np.add(eight_3d[i], [0,0,0])
+            cf.goTo(goal=pt, yaw=0, duration=1.5)
+            time.sleep(1.5)
+        else:
+            pt = np.add(eight_3d[i], [0,0,1])
+            cf.cmdPosition(pos=pt, yaw=0)
+            time.sleep(0.4)
+
+    cf.land(targetHeight = 0.0, duration = 15.0)
+    time.sleep(15.0)
